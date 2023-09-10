60e02581b17ebdb8b4c896b470822e3a9a2d1ad4
Aqsa Younis <aqsayounis2004@gmail.com>
2023-09-10 09:28:43 +0530

    AppleOrange

--- AppleOrange.cpp
+++ AppleOrange.cpp
@@ -1,9 +1,37 @@
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    cout<<"this is a program that i wrote "<<endl;
+    int s,t,a,b,m,n;
+    cin>>s>>t>>a>>b>>m>>n;
+    int arr1[m],arr2[n];   //distances
     
-    return 0;
+    for(int i=0;i<m;i++)
+    {
+        cin>>arr1[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr2[i];
+    }
+    
+    int c1=0,c2=0;
+    for(int i=0;i<m;i++)
+    {
+        if((a+arr1[i])<=t && (a+arr1[i])>=s)
+        {
+            c1++;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if((b+arr2[i])<=t && (b+arr2[i])>=s)
+        {
+            c2++;
+        }
+    }
+    
+    cout<<c1<<endl<<c2;
 }
