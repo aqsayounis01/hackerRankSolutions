bf40e4da49c40d1d1c4e7b847f0508a34542933e
Aqsa Younis <aqsayounis2004@gmail.com>
2023-09-17 13:02:49 +0530

    arrays

---
+++ DivisibleSum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,k,count=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            if(j!=i)
+            {
+                if((a[i]+a[j])%k ==0)
+                {
+                    count++;
+                    
+                }
+            }
+        }
+    }
+    
+    cout<<count;
+}
